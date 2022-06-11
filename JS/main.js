b9ae5f8ddb0fac043d7bb2599cd30ca977c03610
DaniloCaro <94326335+DaniloCaro@users.noreply.github.com>
2022-06-11 09:54:53 -0500

    Se corrigió nav bar dejandola fija y se quitó el scroll en las opciones mostradas en versión mobile. Se agregaron media screen en los archivos ./css/tablet.css y ./css/desktop.css . Se corrigió uso de etiquetas span y div y se cambió por etiquetas img y section en la seccion skills__images.

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,12 @@
 const toogleMenuElement = document.getElementById('nav__toggle-menu');
 const menuElement = document.getElementById('nav__menu');
 const toggleIcon = document.getElementById('toggle-menu__icon');
+const toggleMain = document.getElementById('main')
 
 toogleMenuElement.addEventListener('click', () => {
     menuElement.classList.toggle('nav__menu--show');
     toggleIcon.classList.toggle('toggle-menu__icon--activate');
+    toggleMain.classList.toggle('main--deactivate');
 });
 
 document.getElementById('img2').scrollIntoView(true);
-
